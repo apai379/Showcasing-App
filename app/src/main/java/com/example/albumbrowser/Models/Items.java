b5f7a690477a7280cfbc72e98f38dcf435cabcde
apai379 <79782045+apai379@users.noreply.github.com>
2022-05-26 01:46:32 +1200

    Created Item model

--- app/src/main/java/com/example/albumbrowser/Models/Items.java
+++ app/src/main/java/com/example/albumbrowser/Models/Items.java
@@ -1,4 +1,34 @@
 package com.example.albumbrowser.Models;
 
 public class Items {
+    String albumType, albumName, albumArtist, albumGenre;
+    int albumPrice;
+
+    public Items(String albumType, String albumName, String albumArtist, String albumGenre, int albumPrice) {
+        this.albumType = albumType;
+        this.albumName = albumName;
+        this.albumArtist = albumArtist;
+        this.albumGenre = albumGenre;
+        this.albumPrice = albumPrice;
+    }
+
+    public String getAlbumType() {
+        return albumType;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public String getAlbumArtist() {
+        return albumArtist;
+    }
+
+    public String getAlbumGenre() {
+        return albumGenre;
+    }
+
+    public int getAlbumPrice() {
+        return albumPrice;
+    }
 }
