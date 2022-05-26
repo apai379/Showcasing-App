b14f61142872e2537379cb048e2484b94ea1fffb
apai379 <79782045+apai379@users.noreply.github.com>
2022-05-26 20:19:21 +1200

    Implemented get method for items in ListActivity

--- app/src/main/java/com/example/albumbrowser/DataProvider.java
+++ app/src/main/java/com/example/albumbrowser/DataProvider.java
@@ -1,19 +1,41 @@
 package com.example.albumbrowser;
 
+import com.example.albumbrowser.Models.Items;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class DataProvider {
-    String[] albumName = {"After Hours", "The Dark Side Of The Moon", "After Hours", "The Dark Side Of The Moon",
+    private String[] albumName = {"After Hours", "The Dark Side Of The Moon", "After Hours", "The Dark Side Of The Moon",
     "After Hours", "The Dark Side Of The Moon"};
 
-    String[] albumType = {"Vinyl", "Vinyl", "CD", "CD", "Cassette", "Cassette"};
+    private String[] albumType = {"Vinyl", "Vinyl", "CD", "CD", "Cassette", "Cassette"};
 
-    String[] albumArtist = {"The Weeknd", "Pink Floyd", "The Weeknd", "Pink Floyd", "The Weeknd", "Pink Floyd"};
+    private String[] albumArtist = {"The Weeknd", "Pink Floyd", "The Weeknd", "Pink Floyd", "The Weeknd", "Pink Floyd"};
 
-    String[] albumGenre = {"R&B, New Wave, Dream Pop", "Progressive Rock, Psychedelic", "R&B, New Wave, Dream Pop",
+    private String[] albumGenre = {"R&B, New Wave, Dream Pop", "Progressive Rock, Psychedelic", "R&B, New Wave, Dream Pop",
     "Progressive Rock, Psychedelic", "R&B, New Wave, Dream Pop", "Progressive Rock, Psychedelic"};
 
-    int[] albumRating = {80, 91};
+    private int[] albumRating = {80, 91, 80, 91, 80, 91};
 
-    int[] albumPrice = {45, 65};
+    private int[] albumPrice = {45, 65, 45, 65, 45, 65};
 
-    int[] albumReleaseYear = {2020, 1973};
+    private int[] albumReleaseYear = {2020, 1973, 2020, 1973, 2020, 1973};
+
+
+    public List<Items> getItems(String albumType) {
+        List<Items> itemsList = new LinkedList<Items>();
+        for (int i = 0; i < albumType.length(); i++) {
+            if (this.albumType[i] == albumType) {
+                String itemAlbumName = albumName[i];
+                String itemAlbumArtist = albumArtist[i];
+                String itemAlbumGenre = albumGenre[i];
+                int itemAlbumPrice = albumPrice[i];
+                Items item = new Items(albumType, itemAlbumName, itemAlbumArtist, itemAlbumGenre, itemAlbumPrice);
+                itemsList.add(item);
+            }
+        }
+        return itemsList;
+    }
+
 }
