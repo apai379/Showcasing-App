cd6fb6e8e6242a10e31be6344ffd79642e3ad53a
apai379 <79782045+apai379@users.noreply.github.com>
2022-05-31 19:37:20 +1200

    Fixed recyclerview layout in mainactivity

--- app/src/main/java/com/example/albumbrowser/DetailsActivity.java
+++ app/src/main/java/com/example/albumbrowser/DetailsActivity.java
@@ -12,17 +12,17 @@ public class DetailsActivity<list> {
     private int rating, price, releaseDate, views = 0;
     private String[] imageList = {};
 
-    private List<Items> listOfAll = DataProvider.getItems();
-
-    int i = 0;
-    while (i < listOfAll.length()){
-        Items instance = listOfAll.get(i);
-        this.name = instance.getAlbumName();
-        this.artist = instance.getAlbumArtist();
-        this.genre = instance.getAlbumGenre();
-        //this.imageList = instance.getAlbumIcon();
-        //this.price = instance.getAlbumPrice();
-        //this.rating = instance.
-    }
+//    private List<Items> listOfAll = DataProvider.getItems();
+//
+//    int i = 0;
+//    while (i < listOfAll.length()){
+//        Items instance = listOfAll.get(i);
+//        this.name = instance.getAlbumName();
+//        this.artist = instance.getAlbumArtist();
+//        this.genre = instance.getAlbumGenre();
+//        //this.imageList = instance.getAlbumIcon();
+//        //this.price = instance.getAlbumPrice();
+//        //this.rating = instance.
+//    }
 
 }
